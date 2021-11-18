1579dd396628b0d2a4ea8f966f329d1384956166
Filipe <filipeebueno@gmail.com>
2021-11-18 17:24:59 -0300

    18.11

--- src/br/com/hotel/hotelzao/test/TestGuest.java
+++ src/br/com/hotel/hotelzao/test/TestGuest.java
@@ -8,15 +8,15 @@ public class TestGuest {
 
     public static void main(String[] args) {
 
-        Guest g1 = Guest.create("João", "111.111.111-11", "Rua jiraya",32,"Rua Dezesseis",191,"Guarulhos","SP");
+        Guest g1 = Guest.create("João", "111.111.111-11", "Contador",32,"Rua Dezesseis",191,"Guarulhos","SP");
 
         g1.mostrarInformacoes();
 
-        Guest g2 = Guest.create("Tadeu", "111.111.111-12", "Portero", 31, "Rua zambaloio", 200, "Mezopotamia", "Acre");
+        Guest g2 = Guest.create("Tadeu", "111.111.111-12", "Porteiro", 31, "Rua Zambaloio", 200, "Arujá", "SP");
 
         g2.mostrarInformacoes();
 
-        Guest g3 = Guest.create("Olavo", "222.333.111-02","pro player",19,"Rua joaquim",33,"Rio de janeiro","RJ");
+        Guest g3 = Guest.create("Olavo", "222.333.111-02","pro player",19,"Rua Joaquim",33,"Rio de janeiro","RJ");
         g3.mostrarInformacoes();
 
         System.out.println("Total of Guests: " + Guest.getTotalGuest());
